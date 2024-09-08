9335eb3eb3321fea3105f2e5497428afc17bf833
Ayesha Muskan <Syed.AyeshaMuskanJahan@cognizant.com>
2024-09-09 03:32:12 +0530

    sec

---
+++ App.js
@@ -0,0 +1,31 @@
+import { View, Text, StyleSheet, SafeAreaView, Image,Button, ScrollView } from 'react-native'
+import React from 'react'
+import {NavigationContainer} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import ProductWrapper from './components/ProductWrapper';
+import UserList from './components/UserList';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+
+  
+  return (
+     <NavigationContainer>
+     <Stack.Navigator>
+      <Stack.Screen 
+      name = 'Home'
+      component={ProductWrapper}
+      />
+      <Stack.Screen 
+      name = 'UserList'
+      component={UserList}
+      />
+     </Stack.Navigator>
+     </NavigationContainer>
+  )
+}
+
+
+
+export default App
